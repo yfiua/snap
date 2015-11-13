13eb5e6798f6b444794e880f420ddc68aacad642
yfiua <yfiua@qq.com>
2015-11-13 10:02:35 +0100

    RolX for directed network

---
+++ examples/rolx_directed/Makefile.ex
@@ -0,0 +1,9 @@
+#
+#	configuration variables for the example
+
+## Main application file
+MAIN = testrolx
+DEPH = $(EXSNAPADV)/rolx_directed.h
+DEPCPP = $(EXSNAPADV)/rolx_directed.cpp
+CXXFLAGS += $(CXXOPENMP)
+
